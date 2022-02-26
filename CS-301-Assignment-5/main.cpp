526982399bb6f0dfecb4e556dac4ec4403a32f11
Joshua Dunne <mevsthevoices@gmail.com>
2022-02-25 21:27:28 -0800

    final commit assignment 5

--- CS-301-Assignment-5/main.cpp
+++ CS-301-Assignment-5/main.cpp
@@ -7,14 +7,49 @@
 
 #include "DynamicStack.hpp"
 
-readInFile(DynamicStack&, std::istream& inputFile);
-readOutFile(DynamicStack&, std::ostream& outputFile);
-
 #include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+void readInFile(AS5::DynamicStack<int>&, std::istream& inputFile);
+void readOutFile(AS5::DynamicStack<int>&, std::ostream& outputFile);
+
 
 int main() {
-	static const std::string InputFileName = "a.in";
-	static const std::string OutputFileName = "a.out";
+	AS5::DynamicStack<int> stack;
+
+
+	static const std::string inputFileName = "a.in";
+	static const std::string outputFileName = "a.out";
+
+	std::ifstream inputFile;
+	inputFile.open(inputFileName);
+
+	std::vector<int> lines;
+
+	while (!inputFile.eof()) {
+		int line;
+		inputFile >> line;
+		std::cout << line << std::endl;
+		lines.push_back(line);
+	}
+
+	std::ofstream outputFile;
+	outputFile.open(outputFileName);
+
+	for (auto i : lines) {
+		stack.push(i);
+	}
+
+	while (!stack.isEmpty()) {
+		int popped = stack.pop();
+		outputFile << popped;
+		std::cout << popped << std::endl;
+	}
+
+	return 0;
+
 }
 
 
